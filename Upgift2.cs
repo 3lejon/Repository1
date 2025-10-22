4ff49044278c5a8580980f5d1cfc46947d079fb4
3lejon <Rodny.engblom@gmail.com>
2025-10-22 09:40:48 +0300

    Add files via upload
    Upg2

---
+++ Upgift2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Upg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textBigNumber = "-900000000";
+            long BigNumber = Convert.ToInt64(textBigNumber);
+            Console.WriteLine(BigNumber);
+            Console.WriteLine(BigNumber.GetType());
+            Console.WriteLine(textBigNumber.GetType());
+
+            string numberAsText = "123456";
+            
+
+
+        }
+    }
+}
