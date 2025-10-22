9cd3ae80cf09bce47135434e1c53f76995b71d77
3lejon <Rodny.engblom@gmail.com>
2025-10-22 09:18:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Upg1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jag heter peter");
+            
+            
+            int number = 65;
+            int number2 = 67;
+            
+            Console.WriteLine("int number:\t" + number);
+            Console.WriteLine("int number:\t" + number2);
+
+
+
+
+        }
+    }
+}
