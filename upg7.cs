3574bcd003569cce8028fa5524ea2a4fe889c99e
3lejon <Rodny.engblom@gmail.com>
2025-10-28 12:52:47 +0200

    Update upg7.cs

---
+++ upg7.cs
@@ -0,0 +1,12 @@
+int counter = 1;
+
+     while (counter <= 5)
+    {
+        
+        Console.WriteLine("Counter value: " + counter);
+
+        
+        counter++;
+    }
+
+Console.WriteLine("the loop has finished");
